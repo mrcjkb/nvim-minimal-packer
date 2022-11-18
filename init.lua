c62b3f2da346257cc6951933e9b6673b4ac09d40
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-18 13:31:56 +0100

    minimal config for treesitter-playground and Haskell

--- init.lua
+++ init.lua
@@ -44,6 +44,48 @@ packer.init({
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    requires = { 
+      'nvim-treesitter/playground'
+    },
+    config = function()
+      vim.schedule(function()
+        local ts_configs = require'nvim-treesitter.configs'
+        ts_configs.setup {
+          playground = { -- Toggle with :TSPlaygroundToggle
+            ensure_installed = {'haskell'},
+            auto_install = true, -- Automatically install missing parsers when entering buffer
+            highlight = {
+              enable = true
+            },
+            enable = true,
+            disable = {},
+            updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+            persist_queries = false, -- Whether the query persists across vim sessions
+            keybindings = {
+              toggle_query_editor = 'o',
+              toggle_hl_groups = 'i',
+              toggle_injected_languages = 't',
+              toggle_anonymous_nodes = 'a',
+              toggle_language_display = 'I',
+              focus_language = 'f',
+              unfocus_language = 'F',
+              update = 'R',
+              goto_node = '<cr>',
+              show_help = '?',
+            },
+          },
+          query_linter = {
+            enable = true,
+            use_virtual_text = true,
+            lint_events = {'BufWrite', 'CursorHold'},
+          },
+        }
+      end)
+    end,
+  }
+
   if install_plugins then
     packer.sync()
   end
